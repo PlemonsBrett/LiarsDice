743c695d8825c2aebd59ede73871e1827689c989
Brett-Plemons <brettplemons@outlook.com>
2023-10-14 13:57:49 -0500

    Add comments to make the code more readable in Player.cpp. Swapped recursion validation with a loop in MakeGuess

--- src/model/Player.cpp
+++ src/model/Player.cpp
@@ -1,5 +1,6 @@
 //
 // Created by Brett on 9/4/2023.
+// This file contains the implementation of the Player class, which represents a player in the game of Liar's Dice.
 //
 
 #include "Player.hpp"
@@ -8,16 +9,20 @@
 #include <utility>
 #include <limits>
 
-Player::Player(int id) : id(id), dice(5) {  // Initialize id and reserve space for 5 Dice objects
+// Constructor initializes the player ID and reserves space for 5 dice
+Player::Player(int id) : id(id), dice(5) {
+  // Roll the dice initially for the player
   RollDice();
 }
 
+// Roll all dice for the player
 void Player::RollDice() {
   for (auto& die : dice) {
     die.Roll();
   }
 }
 
+// Display the face values of the player's dice
 void Player::DisplayDice() {
   std::cout << "Player " << id << ", your dice are: ";
   for (const auto& die : dice) {
@@ -26,25 +31,33 @@ void Player::DisplayDice() {
   std::cout << '\n';
 }
 
+// Allow the player to make a guess
 std::pair<int, int> Player::MakeGuess() {
-  std::cout << "Enter your guess in format (quantity, face_value): ";
-  std::string input;
-  std::getline(std::cin, input);
-  std::istringstream iss(input);
+  std::pair<int, int> guess;
+  // Loop until a valid guess is made
+  while (true) {
+    std::cout << "Enter your guess in format (quantity, face_value): ";
+    std::string input;
+    std::getline(std::cin, input);
+    std::istringstream iss(input);
 
-  int quantity, face_value;
-  char comma;
+    int quantity, face_value;
+    char comma;
 
-  if (!(iss >> quantity >> comma >> face_value) || comma != ',') {
+    // Validate the input format
+    if (iss >> quantity >> comma >> face_value && comma == ',') {
+      guess = {quantity, face_value};
+      break;
+    }
+
     std::cout << "Invalid input. Please try again.\n";
     std::cin.clear();
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-    return MakeGuess();
   }
-
-  return {quantity, face_value};
+  return guess;
 }
 
+// Allow the player to call "Liar" on another player's guess
 bool Player::CallLiar() {
   std::cout << "Do you want to call liar? (yes/no) ";
   std::string call_liar;
