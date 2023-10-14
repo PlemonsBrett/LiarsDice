83dc326a6036693636e445328fcb1c453c91661b
Brett-Plemons <brettplemons@outlook.com>
2023-10-14 13:54:58 -0500

    Add comments to describe purpose of each method and member variable in Player.hpp.

--- include/model/Player.hpp
+++ include/model/Player.hpp
@@ -1,5 +1,6 @@
 //
 // Created by Brett on 9/4/2023.
+// This class represents a player in the game of Liar's Dice.
 //
 
 #ifndef PLAYER_HPP
@@ -10,17 +11,27 @@
 
 class Player {
 public:
+  // Constructor initializes the player with an ID
   explicit Player(int id);
 
+  // Rolls all the dice for the player
   void RollDice();
+
+  // Displays the face values of the player's dice
   void DisplayDice();
+
+  // Allows the player to make a guess
   std::pair<int, int> MakeGuess();
+
+  // Allows the player to call "Liar" on another player's guess
   bool CallLiar();
-  [[nodiscard]] const std::vector<Dice>& GetDice() const { return dice; } // Return const reference to avoid copying
+
+  // Returns a const reference to the player's dice to avoid copying
+  [[nodiscard]] const std::vector<Dice>& GetDice() const { return dice; }
 
 private:
-  int id;
-  std::vector<Dice> dice;
+  int id;  // Player ID
+  std::vector<Dice> dice;  // Player's dice
 };
 
 #endif //PLAYER_HPP
