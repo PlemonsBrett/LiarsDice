3f61cd240fca0df4c92304890b95d1e4b4cbc387
Brett-Plemons <brettplemons@outlook.com>
2023-10-14 13:53:47 -0500

    Add descriptive comments to Dice.cpp

--- src/model/Dice.cpp
+++ src/model/Dice.cpp
@@ -1,20 +1,21 @@
 //
 // Created by Brett on 9/4/2023.
+// This file contains the implementation of the Dice class, which represents a dice in the game of Liar's Dice.
 //
 
 #include "Dice.hpp"
 
-// Constructor
+// Constructor initializes the random number generator and rolls the dice
 Dice::Dice() : rd(), gen(rd()), dis(1, 6) {
   Roll();
 }
 
-// Roll method implementation using std::mt19937 and std::uniform_int_distribution
+// Rolls the dice using std::mt19937 and std::uniform_int_distribution
 void Dice::Roll() {
   face_value = dis(gen);
 }
 
-// GetFaceValue method implementation
+// Returns the current face value of the dice
 unsigned int Dice::GetFaceValue() const {
   return face_value;
 }
